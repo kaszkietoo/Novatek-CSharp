fbf8c2bbb1746708d7d4a1f84c07011f48319b93
kaszkietoo <mdomagal@hotmail.com>
2020-02-24 10:26:46 +0100

    CSharp code

---
+++ Novatek.5A/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Novatek._5A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2020;                        
+            
+            for (int month = 1; month <= 12; month++)
+            {
+                for (int day = 1; day <= DateTime.DaysInMonth(year, month); day++)
+                {
+                    Console.WriteLine(GetCompleteString(day, month, year));
+                }
+            }
+        }
+
+        static string GetCompleteString(int day, int month, int year)
+        {
+            int sum = 0;
+            string sDay = day.ToString("00");
+            string sMonth = month.ToString("00");            
+
+            sum = int.Parse(sDay[0].ToString()) + int.Parse(sDay[1].ToString())
+                  + int.Parse(sMonth[0].ToString()) + int.Parse(sMonth[1].ToString()) + 2 + 2;
+
+            sum *= day;
+
+            return string.Format("{0:00}.{1:00}.{2} = {3}", day, month, year, sum);
+        }
+    }
+}
---
+++ Novatek.5BC/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Novatek._5BC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WritePrimeNumbers(1, 1000);
+        }
+
+        static void WritePrimeNumbers(int start, int end)
+        {
+            bool isFirst = true;
+            int first = 0, second = 0;
+
+            for (int i = end; i >= start; i--)
+            {
+                if (IsPrime(i))
+                {
+                    if (isFirst)
+                    {
+                        first = i;
+                        isFirst = false;
+                    }                        
+                    else
+                    {
+                        second = i;
+                        Console.WriteLine("Średnia liczb {0} i {1} = {2:0.00000}", first, second, (first + second) / 2f);
+                        first = second;
+                        
+                    }
+                    Console.WriteLine(i);
+                }                    
+            }            
+        }
+
+        static bool IsPrime(int candidate)
+        {            
+            if ((candidate & 1) == 0)
+            {
+                if (candidate == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            for (int i = 3; (i * i) <= candidate; i += 2)
+            {
+                if ((candidate % i) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return candidate != 1;
+        }
+    }
+}
